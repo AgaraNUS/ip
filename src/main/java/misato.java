8c423bf62c53d853c5547ac3140987917864aa9e
AgaraNUS <e1384320@u.nus.edu>
2026-01-29 12:04:49 +0800

    Level-0

---
+++ src/main/java/misato.java
@@ -0,0 +1,14 @@
+
+
+
+public class misato {
+    public static void main(String[] args) {
+        String end_line = "____________________________________________________________";
+        String name = "Misato Katsuragi";
+        System.out.println(end_line);
+        System.out.println("Hello! I'm " + name + "\n What can I do for you?");
+        System.out.println(end_line);
+        System.out.println("Bye! Hope to see you again soon!");
+        System.out.println(end_line);
+    }
+}
