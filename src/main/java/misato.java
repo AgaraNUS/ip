522c4ca6a0569ec049d1f51a6ace575ebb5073cd
AgaraNUS <e1384320@u.nus.edu>
2026-01-30 17:19:41 +0800

    Level-1
    Implemented echo to chatbot

--- src/main/java/misato.java
+++ src/main/java/misato.java
@@ -1,14 +1,50 @@
+import java.util.Scanner;
 
 
 
 public class misato {
+
     public static void main(String[] args) {
-        String end_line = "____________________________________________________________";
+        //String END_LINE = "____________________________________________________________";
         String name = "Misato Katsuragi";
-        System.out.println(end_line);
-        System.out.println("Hello! I'm " + name + "\n What can I do for you?");
-        System.out.println(end_line);
-        System.out.println("Bye! Hope to see you again soon!");
-        System.out.println(end_line);
+        printLine();
+        System.out.println("Hello! I'm " + name + "\nWhat can I do for you?");
+        printLine();
+
+        //Initialise scanner for user input
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+
+        //Echo loop
+        while (true) {
+            userInput = scanner.nextLine();  //user will type something
+
+            if (userInput.equalsIgnoreCase("bye bye")){
+
+                break;  //exit loop when user types "bye bye"
+            }
+
+            //echo the command back
+            echoCommand(userInput);
+
+
+        }
+
+        printLine();
+        System.out.println("Bye! See you again!");
+        printLine();
+
+        scanner.close();
+    }
+
+    public static void echoCommand(String command) {
+        printLine();
+        System.out.println(command);
+        printLine();
+    }
+
+    public static void printLine() {
+        String END_LINE = "____________________________________________________________";
+        System.out.println(END_LINE);
     }
 }
